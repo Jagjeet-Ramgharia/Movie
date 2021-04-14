c40bb6dea5fe40a1b5696e24ffbf382780798e29
Jagjeet-Ramgharia <ramghariajagjeet4281@gmail.com>
2021-04-14 11:30:19 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp5295950.jpg",
+    title: "HBO Series",
+    sname: "Game Of Thrones",
+    link: "https://www.hbo.com/game-of-thrones",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp5770344.jpg",
+    title: "A Netflix Original Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp6258930.jpg",
+    title: "HBO Series",
+    sname: "The BigBang Theory",
+    link:
+      "https://www.primevideo.com/region/eu/detail/The-Big-Bang-Theory/0PQE65O7V62HMSKNHZJLXRJM6U?ref_=dvm_pds_gen_in_as_s_gt_webp%7Cm_static-c_c511090269595",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp5696666.jpg",
+    title: "A Netflix Original Series",
+    sname: "Stranger Things",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+      imgsrc: "https://wallpapercave.com/wp/wp5110914.jpg",
+      title:"A Netflix Original Series",
+      sname:"The OA",
+      link:"https://www.netflix.com/in/title/80044950"
+  },
+  {
+    imgsrc:"https://wallpapercave.com/wp/wp5696666",
+    title:"A Netflix Original Series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/80057281"
+  }
+];
+export default Sdata;
